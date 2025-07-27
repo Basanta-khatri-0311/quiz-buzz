3ce64506939370ef5187a3d552db7b0d32324b4d
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2025-07-27 22:56:45 +0545

    created a single import file

---
+++ client/src/pages/import.jsx
@@ -0,0 +1,5 @@
+export { default as Home } from './Home';
+export { default as CreateRoom } from './CreateRoom';
+export { default as JoinRoom } from './JoinRoom';
+export { default as Quiz } from './Quiz';
+export { default as Result } from './Result';
