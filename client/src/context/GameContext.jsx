3888bcb8b5afba5e5046775f99ec890043178cc4
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2025-07-27 22:57:39 +0545

    created game context

---
+++ client/src/context/GameContext.jsx
@@ -0,0 +1,26 @@
+import { createContext, useContext, useState } from "react";
+
+const GameContext = createContext();
+
+export const GameProvider = ({ children }) => {
+  const [username, setUsername] = useState("");
+  const [category, setCategory] = useState("");
+  const [roomCode, setRoomCode] = useState("");
+
+  return (
+    <GameContext.Provider
+      value={{
+        username,
+        setUsername,
+        category,
+        setCategory,
+        roomCode,
+        setRoomCode,
+      }}
+    >
+      {children}
+    </GameContext.Provider>
+  );
+};
+
+export const useGame = () => useContext(GameContext);
