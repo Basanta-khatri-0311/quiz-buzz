5eca607240289c12c742cd8a785250e063c807a9
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2025-07-27 22:09:21 +0545

    Initial frontend and backend setup

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="text-center p-10">
+      <h1 className="text-3xl font-bold text-purple-700">Welcome to QuizBuzz!</h1>
+    </div>
+  )
+}
+
+export default App
