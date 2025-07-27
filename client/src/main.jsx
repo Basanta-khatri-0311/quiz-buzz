521d499fef7d3dade0d05a4102c38908d9230c31
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2025-07-27 23:31:47 +0545

    wrapped the app with quiz context provider for data asharing

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { GameProvider } from "./context/GameContext.jsx";
+import { QuizProvider } from "./context/QuizContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <GameProvider>
-    <StrictMode>
-      <App />
-    </StrictMode>
+    <QuizProvider>
+      <StrictMode>
+        <App />
+      </StrictMode>
+    </QuizProvider>
   </GameProvider>
 );
